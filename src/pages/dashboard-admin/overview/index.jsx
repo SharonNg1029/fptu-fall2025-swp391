ccfc859ae9eccc7d145affdf5599765010776aa4
Ngan Nguyen <saoaz1029@gmail.com>
2025-06-25 01:38:51 +0700

    adjust overview dashboard

--- src/pages/dashboard-admin/overview/index.jsx
+++ src/pages/dashboard-admin/overview/index.jsx
@@ -300,7 +300,7 @@ const Overview = () => {
     } finally {
       setLoading(false);
     }
-  }, [dateRange]);
+  }, [dateRange, fetchTotalCustomers, fetchCompletedTests, fetchKitsSold]);
 
   // Generate chart data when stats or bookings change
   useEffect(() => {
